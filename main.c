8df4efbc12c35d0ff2db69e54d727c3af189c21b
Alpe83 <peri.alessandro@gmail.com>
2019-03-01 17:00:49 -0700

    Initial Init

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    
+    // Declare the data
+    int a;
+    a = 5;
+    printf("%d\n",a);
+    printf("Gimme a number dude:\n");
+    scanf("%d",&a);
+    printf("%c\n",a);
+    
+    return 1;
+}
