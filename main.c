59df896e15865e5a5a36b04308ced04bdacbf771
Alpe83 <peri.alessandro@gmail.com>
2020-03-04 10:04:21 -0700

    hello world: done!

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 int main(){
     
-    // Declare the data
-    int a;
-    a = 5;
-    printf("%d\n",a);
-    printf("Gimme a number dude:\n");
-    scanf("%d",&a);
-    printf("%c\n",a);
-    
-    
-    
-    
+    printf("Hello World!");
     
     return 1;
 }
+
