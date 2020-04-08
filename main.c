a7cb4c15b13526be739f67d9f9bfb4384c2f0e49
Alpe83 <peri.alessandro@gmail.com>
2020-04-08 10:22:38 -0600

    Study of Files

--- main.c
+++ main.c
@@ -22,7 +22,25 @@ int main(){
     printf("What is i after the second bracket: %d\n",i);
     
     
+    FILE *fp; // Pointer to a File! Store address to FILE in your HD
     
+    fp = fopen("/Users/alessandroperi/GoogleDrive/BOULDER/Teaching/Macroeconomics-1-Phd/C-Programming/C-MACRO/C-MACRO/README.txt","r"); // Getting the Address
+    // fopen(Address of the File,'Mode of Opening') define in stdio
+    // Mode of Opening: read(r) write (w), append(a)
+    if(fp==NULL){
+        printf("Imbecil you made a mistake!");
+        exit(-1);
+    }
+    
+    double a;
+    fscanf(fp,"%lf",&a);
+    
+    printf("What is the number: %lf\n",a);
+    
+    fprintf(stdout,"What is the number print it on screen: %lf\n",a);
+    
+    
+    fclose(fp);
     return 1;
 }
 
